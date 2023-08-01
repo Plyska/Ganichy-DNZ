b9feb989782ec0196848182260d35caba188d2c7
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-08-01 15:34:14 +0200

    add routes

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello Home!</div>,
+  },
+  {
+    path: "/about-us",
+    element: <div>Hello About Us!</div>,
+  },
+]);
+
+const AppRouter = () => <RouterProvider router={router} />
+
+export default AppRouter;
